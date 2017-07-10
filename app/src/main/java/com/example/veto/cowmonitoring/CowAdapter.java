251f8b66ec8e31332e543e51020bf8210a2cfb11
YaraAHakiim <yara.ahakiim@gmail.com>
2017-07-10 16:11:51 +0200

    added monitor fragement

---
+++ app/src/main/java/com/example/veto/cowmonitoring/CowAdapter.java
@@ -0,0 +1,62 @@
+package com.example.veto.cowmonitoring;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by YaRa on 7/10/2017.
+ */
+
+public class CowAdapter extends RecyclerView.Adapter<CowAdapter.MyViewHolder> {
+
+    Context context;
+    List<String> dataSet;
+    private MyViewHolder holder;
+    private int position;
+    View view ;
+
+    public CowAdapter(Context context, List<String> data)
+    {
+
+        this.context = context;
+        dataSet= data;
+
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        view = LayoutInflater.from(context).inflate(R.layout.custom_view,parent,false);
+        MyViewHolder viewHolder = new MyViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+
+        holder.cowId.setText(dataSet.get(position));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataSet.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView cowId;
+
+
+
+        public MyViewHolder(View view) {
+            super(view);
+            cowId = (TextView) view.findViewById(R.id.cowId);
+        }
+    }
+}
--- app/src/main/java/com/example/veto/cowmonitoring/MonitorFragment.java
+++ app/src/main/java/com/example/veto/cowmonitoring/MonitorFragment.java
@@ -1,17 +1,41 @@
 package com.example.veto.cowmonitoring;
 
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import static android.content.Context.MODE_PRIVATE;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MonitorFragment extends Fragment {
+
+    View rootView ;
+
+    Context context ;
+    LinearLayout linearLayout;
+    RecyclerView recyclerView;
+    RecyclerView.Adapter adapter;
+    RecyclerView.LayoutManager layoutManager;
 
 
     public MonitorFragment() {
@@ -22,8 +46,66 @@ public class MonitorFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_monitor, container, false);
+
+        initializeViews(inflater,container);
+        prepareRecyclerView();
+
+
+        return rootView ;
     }
+
+    void initializeViews (LayoutInflater inflater, ViewGroup container)
+    {
+        rootView = inflater.inflate(R.layout.fragment_monitor, container, false);
+
+        context = getContext();
+        linearLayout = (LinearLayout) rootView.findViewById(R.id.linearLayout);
+        recyclerView = (RecyclerView) rootView.findViewById(R.id.recyclerView);
+
+    }
+
+    void prepareRecyclerView ()
+    {
+        layoutManager = new LinearLayoutManager(context);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(new CowAdapter(context,getFarmNodes()));
+    }
+
+    List<String> getFarmNodes ()
+    {
+        List<String> nodes ;
+
+        HttpGet httpGet = new HttpGet();
+
+        SharedPreferences prefs = getContext().getSharedPreferences("Login session", MODE_PRIVATE);
+        String user = prefs.getString("Current User",null);
+
+        String farmId = new String();
+
+        Gson gson = new Gson() ;
+
+        User currentUser = gson.fromJson(user , User.class);
+
+
+        if (currentUser != null) {
+            farmId = currentUser.getFarmId();
+        }
+
+        String url = "getFarmNodes?farmId="+farmId;
+        String nodesString = "";
+
+        try {
+            nodesString = httpGet.execute(url).get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        nodes = gson.fromJson(nodesString,new TypeToken<ArrayList<String>>(){}.getType());
+        Log.d("node" , nodes.get(0));
+        return nodes;
+    }
+
 
 }
